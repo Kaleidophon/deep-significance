cd017c69c77aca79d264aeb9cdf43355e339215e
Kaleidophon <dennis.ulmer@gmx.de>
2021-03-17 17:40:38 +0100

    :bug: Trying another hacky fix for broken equations on readthedocs

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -1,5 +1,5 @@
 cat ../README.md | sed 's/:\([a-z_]*\):/|:\1:|/g' > ./README_DOCS.md
-cp -r ../svgs/ ./svgs
 cp -r ../img/ ./img
-sed -i "" 's/svgs/\.\.\/\.\.\/svgs/g' README_DOCS.md
+cp -r ../svgs/ ./_images/
+sed -i "" 's/svgs/\.\.\/\.\.\/\.\.\/svgs/g' README_DOCS.md
 make clean && make html
