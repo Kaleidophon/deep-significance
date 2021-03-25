c11026560e91cdbd91ede9a715b26468e3390c6c
Kaleidophon <dennis.ulmer@gmx.de>
2021-03-25 16:27:16 +0100

    :tada: Set official package name to deepsig

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="deep-signifcance",  # Replace with your own username
+    name="deepsig",
     version="1.0.0",
     author="Dennis Ulmer",
     description="Easy Significance Testing for Deep Neural Networks.",
