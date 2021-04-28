b20ea9ba27d0814611d397a46c532d75ccdce83f
Kaleidophon <dennis.ulmer@gmx.de>
2021-04-28 14:56:57 +0200

    :books: Increment version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="deepsig",
-    version="1.0.0",
+    version="1.0.1",
     author="Dennis Ulmer",
     description="Easy Significance Testing for Deep Neural Networks.",
     long_description=long_description,
